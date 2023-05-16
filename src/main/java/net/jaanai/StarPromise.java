419c191a9e3e6931dffbd5bfae59895589f94078
Jaanai <Jaanai1531619138@gmail.com>
2023-05-16 12:58:32 +0800

    Add Ceres

--- src/main/java/net/jaanai/StarPromise.java
+++ src/main/java/net/jaanai/StarPromise.java
@@ -1,10 +1,19 @@
 package net.jaanai;
 
 import net.fabricmc.api.ModInitializer;
+import net.jaanai.content.item.weapon.sword.CeresToolMaterial;
+import net.minecraft.item.Item;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolItem;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
+import net.minecraft.util.registry.Registry;
 
 public class StarPromise implements ModInitializer {
+    public static final ToolItem CERES = new SwordItem(CeresToolMaterial.INSTANCE, 3, -2.6F, new Item.Settings().rarity(Rarity.EPIC).fireproof());
+
     @Override
     public void onInitialize() {
-
+        Registry.register(Registry.ITEM, new Identifier("star_promise", "ceres"), CERES);
     }
 }
---
+++ src/main/java/net/jaanai/content/item/weapon/sword/CeresToolMaterial.java
@@ -0,0 +1,39 @@
+package net.jaanai.content.item.weapon.sword;
+
+import net.minecraft.item.Items;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+
+public class CeresToolMaterial implements ToolMaterial {
+    public static final CeresToolMaterial INSTANCE = new CeresToolMaterial();
+
+    @Override
+    public int getDurability() {
+        return 5000;
+    }
+
+    @Override
+    public float getMiningSpeedMultiplier() {
+        return 12.0F;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return 8.0F;
+    }
+
+    @Override
+    public int getMiningLevel() {
+        return 3;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 50;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.ofItems(Items.DIAMOND);
+    }
+}
